9c52d2efb756b51d46b4fe25f233ec720d37798f
ngesa marvin <marvinngesa@gmail.com>
2018-01-19 21:16:23 +0300

    add a log so to enable us see the app running

--- app/src/main/java/com/droidmarvin/gettingstartedwithandroidthings/MainActivity.java
+++ app/src/main/java/com/droidmarvin/gettingstartedwithandroidthings/MainActivity.java
@@ -2,11 +2,13 @@ package com.droidmarvin.gettingstartedwithandroidthings;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("TUT", "Hello Things");
     }
 }
